48985c4f0d375e990f5102bc76e6e7fb04a92b45
Cody Ciaschi <cciaschi1224@gmail.com>
2022-09-02 15:30:01 -0400

    Rebase

--- lua/conf/init.lua
+++ lua/conf/init.lua
@@ -4,9 +4,6 @@ require("conf.neoscroll")
 require("conf.indent-blankline")
 require("conf.nvim-treesitter")
 require("conf.nvim-ts-autotag")
-<<<<<<< HEAD
-=======
 require("conf.nvim-autopairs")
 require("conf.neogit")
 vim.cmd('source /home/cody/.config/nvim/lua/conf/coc.vim')
->>>>>>> d9cdee2 (Added CoC and removed cmp)
